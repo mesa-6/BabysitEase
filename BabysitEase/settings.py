7d0b4a02e74f803bd5a2a0a571ff69df486452b8
Gheyson Melo <gheysonmelo@hotmail.com>
2024-03-20 11:46:47 -0300

    fix: re-adjusting folder name

--- BabysitEase/settings.py
+++ BabysitEase/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'base.urls'
+ROOT_URLCONF = 'BabysitEase.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'base.wsgi.application'
+WSGI_APPLICATION = 'BabysitEase.wsgi.application'
 
 
 # Database
