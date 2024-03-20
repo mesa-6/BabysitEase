da09082d85cd6dfa8430a39b00b104505ae556a0
Gheyson Melo <gheysonmelo@hotmail.com>
2024-03-20 11:27:17 -0300

    refactor: switching name of base folder to not confuse

--- base/settings.py
+++ base/settings.py
@@ -49,7 +49,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'BabysitEase.urls'
+ROOT_URLCONF = 'base.urls'
 
 TEMPLATES = [
     {
@@ -67,7 +67,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'BabysitEase.wsgi.application'
+WSGI_APPLICATION = 'base.wsgi.application'
 
 
 # Database
