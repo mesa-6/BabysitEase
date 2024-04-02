d3f713e9b282bf2274f5389db421fa1dac30fb6a
ArthurCapistrano <arthur.capistrano12@gmail.com>
2024-04-02 14:45:49 -0300

    refactor: moving home and room to a new folder in base folder

--- base/views.py
+++ base/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, 'home.html')
+    return render(request, 'base/home.html')
 
-def room(request):
-    return render(request, 'room.html')
+def room(request, pk):
+    return render(request, 'base/room.html')
