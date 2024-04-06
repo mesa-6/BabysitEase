5d21bb7c30fab50b12a3e08d1af423e4900f52df
Gheyson Melo <gheysonmelo@hotmail.com>
2024-04-06 12:58:55 -0300

    remove: staticfiles_dir environment variable

--- BabysitEase/BabysitEase/settings.py
+++ BabysitEase/BabysitEase/settings.py
@@ -144,11 +144,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 STATICFILES_STORAGE = ('whitenoise.storage.CompressedManifestStaticFilesStorage')
 
-STATICFILES_DIRS = [
-    
-    BASE_DIR / 'static',
-    
-]
 
 
 # Default primary key field type
