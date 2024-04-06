20b70a4fc4cc68fce12ec9a0fd4abbecad09d91e
ArthurCapistrano <arthur.capistrano12@gmail.com>
2024-04-06 17:51:03 -0300

    add: Babyssiter, Parent and favorite to admin site

--- BabysitEase/base/admin.py
+++ BabysitEase/base/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Babysitter, Parent, Favorite
 
 # Register your models here.
+admin.site.register(Babysitter)
+admin.site.register(Parent)
+admin.site.register(Favorite)
