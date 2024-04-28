6e22dbf46c5773d7d60fb88931abbdbf826d568c
Arthur silva Capistrano <arthur.capistrano12@gmail.com>
2024-04-28 11:34:08 -0300

    refactor: changing babysitterDetails

--- BabysitEase/base/static/styles/details.js
+++ BabysitEase/base/static/styles/details.js
@@ -1,8 +1,3 @@
-const hamBurger = document.querySelector(".toggle-btn");
-
-hamBurger.addEventListener("click", function () {
-  document.querySelector("#sidebar").classList.toggle("expand");
-});
 
 
 function myFunction() {
