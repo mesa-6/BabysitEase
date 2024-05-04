fac5812acc068e80ce2f1f2c77c143fb6181b75a
Gheyson Melo <gheysonmelo@hotmail.com>
2024-05-04 14:04:22 -0300

    .

--- BabysitEase/cypress/support/e2e.js
+++ BabysitEase/cypress/support/e2e.js
@@ -14,8 +14,10 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import './commands';
 require('./pages/home.pages')
+require('./pages/cadastro.pages')
+require('./pages/login.pages')
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
