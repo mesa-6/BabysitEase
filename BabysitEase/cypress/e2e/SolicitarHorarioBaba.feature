3f0aa4b2214d707df6dd5e389e8143ad34f1cb66
Arthur silva Capistrano <arthur.capistrano12@gmail.com>
2024-05-05 11:42:30 -0300

    add: SolicitarHorario Test without select a schedule

--- BabysitEase/cypress/e2e/SolicitarHorarioBaba.feature
+++ BabysitEase/cypress/e2e/SolicitarHorarioBaba.feature
@@ -7,3 +7,9 @@ Funcionalidade: Enviar Solicitação de Horário à baba
     E estou na pagina "Detalhes da Baba"
     Quando eu selecionar um horário disponível, e enviar e confirmar a solicitação
     Então devo visualizar o horário selecionado como "Pendente"
+
+Cenário: Enviar Solicitação à babá sem selecionar horário
+    Dado que tenho login em um perfil de usuário 
+    E estou na pagina "Detalhes da Baba"
+    Quando eu selecionar botão de Solicitar e confirmar sem antes selecionar um horário 
+    Então devo visualizar mensagem "Por favor, selecione pelo menos um horário."
--- BabysitEase/cypress/support/step-definitions/SolicitarHorarios.cy.js
+++ BabysitEase/cypress/support/step-definitions/SolicitarHorarios.cy.js
@@ -1,8 +1,5 @@
 const { Given, And, When, Then } = require("cypress-cucumber-preprocessor/lib/resolveStepDefinition")
 
-let horarioEncontrado = false;
-let idHorarioDisponivel = '';
-
 // Cenário 1
 Given('que tenho um perfil de usuário', () => {
     cy.visit('/register')
@@ -48,3 +45,32 @@ Given('que tenho um perfil de usuário', () => {
 
 });
 
+// Cenário 2
+Given('que tenho login em um perfil de usuário', () => {
+    cy.visit('/login')
+    cy.login('tiago', 
+    '1' )
+    
+  });
+  
+  And('estou na pagina "Detalhes da Baba"', () => {
+    cy.get('#SemBabasDisponiveis').should('not.exist');
+
+    cy.get('.card.h-100').first().within(() => {
+        cy.get('.btn-readmore').click();
+      });
+
+  });
+  When('eu selecionar botão de Solicitar e confirmar sem antes selecionar um horário', () => {
+    
+    cy.get('#confirmButton').click();
+    cy.get('#confirmModal').within(() => {
+        cy.get('#sendDataButton').click({ force: true });
+      });
+
+});
+
+  Then('devo visualizar mensagem "Por favor, selecione pelo menos um horário."', () => {
+    cy.get('#aoMenosUmHorario').should('contain', 'Por favor, selecione pelo menos um horário.');
+
+});
